dbc4663c8a635ecc88d457bc0110edf358f6009d
Arturo Rinaldi <arty.net2@gmail.com>
2017-06-21 11:19:28 +0200

    x86 : adding linino-x86 specific packages

---
+++ package/linino-x86/opencv3/opencv-3.1.0/modules/photo/.photo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/photo
+# Build directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/photo/.photo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
