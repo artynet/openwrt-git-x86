92983460603ecac74f182c00219bb89b3caa037a
Arturo Rinaldi <arty.net2@gmail.com>
2017-06-21 11:19:28 +0200

    x86 : adding linino-x86 specific packages

---
+++ package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_cm.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_cm.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_debug.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_debug.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_getopt.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_getopt.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_icc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_icc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_iccdata.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_iccdata.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_image.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_image.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_init.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_init.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_malloc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_malloc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_seq.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_seq.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_stream.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_stream.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_string.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_string.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_tmr.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_tmr.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_tvp.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_tvp.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jas_version.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jas_version.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jp2_cod.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jp2_cod.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jp2_dec.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jp2_dec.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jp2_enc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jp2_enc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_bs.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_bs.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_cs.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_cs.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_dec.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_dec.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_enc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_enc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_math.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_math.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_mct.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_mct.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_mqcod.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_mqcod.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_mqdec.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_mqdec.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_mqenc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_mqenc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_qmfb.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_qmfb.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t1cod.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t1cod.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t1dec.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t1dec.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t1enc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t1enc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t2cod.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t2cod.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t2dec.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t2dec.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_t2enc.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_t2enc.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_tagtree.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_tagtree.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_tsfb.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_tsfb.c.o"
+  "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/jpc_util.c" "/home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/3rdparty/libjasper/CMakeFiles/libjasper.dir/jpc_util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "EXCLUDE_BMP_SUPPORT"
+  "EXCLUDE_JPG_SUPPORT"
+  "EXCLUDE_MIF_SUPPORT"
+  "EXCLUDE_PGX_SUPPORT"
+  "EXCLUDE_PNM_SUPPORT"
+  "EXCLUDE_RAS_SUPPORT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "3rdparty/libjasper"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ package/linino-x86/opencv3/opencv-3.1.0/modules/cudaimgproc/.cudaimgproc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/cudaimgproc
+# Build directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/cudaimgproc/.cudaimgproc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
