a8d6092a8fcd2b78830e050cae761e1e99660755
Arturo Rinaldi <arty.net2@gmail.com>
2018-06-09 00:50:32 +0200

    opencv : update to release v3.4.1

--- package/linino-x86/opencv3/opencv-3.1.0/modules/imgcodecs/.imgcodecs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/imgcodecs
-# Build directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/imgcodecs/.imgcodecs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- package/linino-x86/opencv3/opencv-3.1.0/modules/python/.python/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/python
-# Build directory: /home/arturo/Scaricati/openwrt-15.05-x86/package/linino-x86/opencv3/opencv-3.1.0/modules/python/.python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(python2)
-subdirs(python3)
